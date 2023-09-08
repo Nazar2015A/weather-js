590e92790fc6c19ee2bbdbb88cfcb06f7cf5536a
Nazar2015A <nazar.komar456@gmail.com>
2023-09-08 22:20:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ const DAYS = {
 
 window.onload = function () {
   window.setInterval(function () {
-    var now = new Date();
-    var clock = document.getElementById("clock");
+    const now = new Date();
+    const clock = document.getElementById("clock");
     clock.innerHTML = now.toLocaleTimeString();
   }, 1000);
 };
